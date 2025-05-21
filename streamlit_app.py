af2cec2d88053dd090f8b8fac6240f120d511524
raffarei <rafchafez@gmail.com>
2025-05-21 14:10:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 Sunan Ampel")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG20250502193758.jpg",width=200)
